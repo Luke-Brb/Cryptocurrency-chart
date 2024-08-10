6cd8b078ec8083d70c436c2bd0d79c8edfa3fbce
Lucian Barbu <112772067+Luke-Brb@users.noreply.github.com>
2024-08-10 23:44:41 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -68,30 +68,3 @@ const App = () => {
 };
 
 export default App;
-
-// export default function App() {
-//   return (
-//     <>
-//       <nav className="navbar navbar-dark bg-dark">
-//         <div className="container-fluid justify-content-center">
-//           <h1 className="navbar-brand mb-2 fs-2">
-//             <strong>Cryptocurrency chart</strong>
-//           </h1>
-//         </div>
-//       </nav>
-//       <div className="d-flex align-items-center justify-content-center h-100">
-//         <div className="container rounded w-75 border border-3 border-dark bg-light mx-auto mt-5">
-//           <div className="d-flex justify-content-between mx-4 mt-3 mb-3">
-//             <DropdownButtons />
-//             <Datepicker />
-//             <Intervals />
-//           </div>
-//           <div>
-//             <Charts />
-//             <MyChart />
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
