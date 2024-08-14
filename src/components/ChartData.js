90768978ecfcf7c324c3ad21f571979cb70fdea8
Lucian Barbu <112772067+Luke-Brb@users.noreply.github.com>
2024-08-14 23:15:07 +0300

    Update ChartData.js

--- src/components/ChartData.js
+++ src/components/ChartData.js
@@ -2,10 +2,7 @@ import React from "react";
 import MyChart from "./MyChart";
 
 const ChartData = ({ selectedCoin, startDate, endDate }) => {
-  console.log("ChartData - selectedCoin:", selectedCoin);
-  console.log("ChartData - startDate:", startDate);
-  console.log("ChartData - endDate:", endDate);
-
+  
   return (
     <MyChart coinID={selectedCoin} startDate={startDate} endDate={endDate} />
   );
