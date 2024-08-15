9156338d23b26194bf02bc4934ceec4d5024096b
Lucian Barbu <112772067+Luke-Brb@users.noreply.github.com>
2024-08-15 19:36:35 +0300

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,17 +1,12 @@
 import axios from "axios";
 
 export async function fetchData(coinId, startDate, endDate) {
-  console.log("API coinId:", coinId);
-  console.log("API startDate:", startDate);
-  console.log("API endDate:", endDate);
   const dayStart = startDate.getTime() / 1000;
   const dayEnd = endDate.getTime() / 1000;
   if (Math.floor(dayStart) == Math.floor(dayEnd)) {
     startDate = endDate.getTime() - 300000;
   }
 
-  console.log(" *** startDate :", " ", startDate);
-  console.log(" *** endDate :", endDate.getTime(), " ", endDate);
   try {
     const options = {
       method: "GET",
@@ -28,7 +23,6 @@ export async function fetchData(coinId, startDate, endDate) {
       },
     };
     const response = await axios.request(options);
-    console.log("API - return response:", response.data);
     return response;
   } catch (error) {
     console.error(error);
